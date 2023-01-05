19fc7ee59d554c58adfb808cf38af489408a7792
jetoro <76423923+pl0wox@users.noreply.github.com>
2023-01-05 23:00:17 +0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jethro Roxas on 1/5/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
